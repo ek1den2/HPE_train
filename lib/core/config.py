60da58b4d76963eb744d4668d5ffe5d1dbb9b9ca
ek1den2 <masuryui0829@gmail.com>
2025-11-01 12:57:34 +0900

    -fix: mean, stdを追加

--- lib/core/config.py
+++ lib/core/config.py
@@ -53,10 +53,14 @@ config.DATASET = edict()
 config.DATASET.ROOT = ''
 config.DATASET.DATASET = 'mpii'
 config.DATASET.TRAIN_SET = 'train'
-config.DATASET.TEST_SET = 'valid'
-config.DATASET.DATA_FORMAT = 'jpg'
+config.DATASET.VAL_SET = 'valid'
+config.DATASET.TEST_SET = 'test'
+config.DATASET.DATA_FORMAT = 'png'
 config.DATASET.HYBRID_JOINTS_TYPE = ''
 config.DATASET.SELECT_DATA = False
+
+config.DATASET.MEAN = [0.5]
+config.DATASET.STD = [0.5]
 
 # training data augmentation
 config.DATASET.FLIP = True
