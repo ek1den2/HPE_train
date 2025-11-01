42eddad8ca4e6f092f1473017c240a71a5eb33e4
ek1den2 <masuryui0829@gmail.com>
2025-11-01 12:58:44 +0900

    -add: モデルステータスの確認

--- lib/network/networks.py
+++ lib/network/networks.py
@@ -8,3 +8,29 @@ def get_pose_net(cfg, is_train):
         return pose_mobilenet.get_model(cfg, is_train)
     else:
         raise ValueError(f"モデル名がサポートされていません: {cfg.MODEL.NAME}")
+    
+
+
+
+if __name__ == '__main__':
+    import torch
+    import thop
+    from train import parse_args, reset_config
+    from lib.core.config import config
+    from torch.utils.tensorboard import SummaryWriter
+    args = parse_args()
+    reset_config(config, args)
+
+    model = get_pose_net(config, is_train=True)
+
+    print(model)
+
+    dummy_input = torch.randn(1, 1, 160, 160)
+    flops, params = thop.profile(model, inputs=(dummy_input, ))
+    gflops = flops / 1e9
+    print(f"FLOPs: {flops} ({gflops:.2f} GFLOPs)")
+    print(f"Parameters: {params}")
+
+    writer = SummaryWriter("experiments/tbX/")
+    writer.add_graph(model, (dummy_input, ))
+    writer.close()
